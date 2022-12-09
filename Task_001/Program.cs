772898450436e6bc54279d80c4893d922a29a403
Dmitry Kizimov <kizimov@mail.ru>
2022-12-10 02:01:14 +0300

    Task 1 is OK

---
+++ Task_001/Program.cs
@@ -0,0 +1,16 @@
+﻿// число a возводим в степень b
+// принимаем на вход два числа - а и b
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Мы будем возводить число А в натуральную степень В,");
+Console.Write("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+            for (int i = 0; i < B; i++)
+            {
+                result *= A;
+            }
+Console.WriteLine(result);
